a5ee8898b53c7d8ea4065b5309e0953bba0c6b54
JPaulGrayson <j.paul.grayson@gmail.com>
2026-01-18 14:04:03 -0600

    Add client/src/components/process/index.ts

---
+++ client/src/components/process/index.ts
@@ -0,0 +1,9 @@
+/**
+ * LogicProcess Components
+ */
+
+export { ProcessNode } from './ProcessNode';
+export { SwimlaneLane } from './SwimlaneLane';
+export { SwimlaneDiagram } from './SwimlaneDiagram';
+export { ProcessInput } from './ProcessInput';
+export { RoleManager } from './RoleManager';
